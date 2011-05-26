454a6b5a3b1a6b2902168f48ffaa49bb3d9d2d39
77web <info@77-web.com>
2011-05-26 12:10:01 +0900

    notify event in particular clap count

--- lib/action/opClapPluginClapActions.class.php
+++ lib/action/opClapPluginClapActions.class.php
@@ -9,6 +9,13 @@ class opClapPluginClapActions extends sfActions
     if($this->form->isValid())
     {
       $clap = $this->form->save();
+      
+      $alerts = sfConfig::get('app_clap_count_alerts', array());
+      if(count($alerts)>0 && in_array($clap->getClaps(), $alerts))
+      {
+        sfContext::getInstance()->getEventDispatcher()->notify(new sfEvent($this, 'opClapPlugin.clapAlert', array('clap'=>$clap)));
+      }
+      
       $this->redirect($clap->getForeignUrl());
       return;
     }
